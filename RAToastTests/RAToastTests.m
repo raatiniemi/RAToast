dedd8d5ade847f975229a952e528af7caf15bd1e
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-06-13 23:30:31 +0200

    Initial commit

---
+++ RAToastTests/RAToastTests.m
@@ -0,0 +1,17 @@
+//
+//  RAToastTests.m
+//  RAToastTests
+//
+//  Created by Tobias Raatiniemi on 2014-06-13.
+//  Copyright (c) 2014 Raatiniemi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RAToastTests : XCTestCase
+
+@end
+
+@implementation RAToastTests
+
+@end
